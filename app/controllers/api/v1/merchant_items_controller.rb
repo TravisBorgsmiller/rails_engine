e6872bbbcc12800f2041244a9aa33f21fb78a3e1
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-05-20 23:20:41 -0500

    add relationships controller for merchants items

---
+++ app/controllers/api/v1/merchant_items_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::MerchantItemsController < ApplicationController
+
+  def index
+    @merchant = Merchant.find(params[:id])
+    render json: ItemSerializer.new(@merchant.items)
+  end
+
+end
